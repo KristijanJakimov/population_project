a4447518468212d3039427cab541910ed36419a5
Kristijan Jakimov <kj60401@student.uni-lj.si>
2026-08-21 16:16:20 +0200

    main to include regions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from helpers import fetch_page, save_to_csv
 from countries_scraper import scrape_countries
+from regions_scraper import scrape_region
 
 COUNTRIES_URL = "https://www.worldometers.info/world-population/population-by-country/"
 COUNTRY_FIELDS = ["rank", "country", "population", "yearly_change", "net_change","density", "land_area", "migrants", "fertility_rate", "median_age", "urban_pop", "world_share"]
@@ -7,3 +8,11 @@ COUNTRY_FIELDS = ["rank", "country", "population", "yearly_change", "net_change"
 html = fetch_page(COUNTRIES_URL)
 countries = scrape_countries(html)
 save_to_csv(countries, "countries.csv", COUNTRY_FIELDS)
+
+REGION_FIELDS = ["country", "continent"]
+
+regions = []
+for continent, url in REGION_URLS.items():
+    region_html = fetch_page(url)
+    regions.extend(scrape_region(region_html, continent))
+save_to_csv(regions, "regions.csv", REGION_FIELDS)
